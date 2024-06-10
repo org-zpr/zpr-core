05872fdf981c26e366e2444dfc746b5d25923a05
owenrhysthomas <othomas@appliedinvention.com>
2024-06-10 13:34:09 -0400

    Added ability to respond to ECHO command

--- adapter/ph/src/rpc_worker.rs
+++ adapter/ph/src/rpc_worker.rs
@@ -8,6 +8,13 @@ use crate::packet::Packet;
 use tokio::net::UnixListener;
 use tokio::net::UnixStream;
 use tokio::io::AsyncWriteExt;
+use std::io::prelude::*;
+use tokio::io::BufReader;
+use tokio::io::BufWriter;
+
+use std::fs::File;
+use std::thread;
+use tokio::io::AsyncBufReadExt;
 
 #[derive(Copy, Clone)]
 pub struct Config {
@@ -22,7 +29,24 @@ async fn worker(
         match socket.accept().await {
             Ok((mut stream, _addr)) => {
                 eprintln!("Connection recieved");
-                stream.shutdown();
+                let mut str_message = String::new();
+                let mut split_buf = stream.split(); // split stream into read/write streams
+                let mut buf_reader = BufReader::new(split_buf.0);
+                let mut buf_writer = BufWriter::new(split_buf.1);
+                buf_reader.read_line(&mut str_message).await;
+                str_message.pop(); // Removes \n from end of string
+
+                // TODO remove \n from end of message?
+                buf_writer.write("Message Recieved\n".as_bytes()).await;
+
+                match str_message.as_str() {
+                    "ECHO" => {buf_writer.write_all(echo(asm).await.as_bytes()).await;
+                                  buf_writer.write_all("OK\n".as_bytes()).await}, 
+                    _      => buf_writer.write_all("ERR\n".as_bytes()).await,
+                };
+
+                buf_writer.flush().await;
+                buf_writer.shutdown().await;
             }
             Err(e) => {
                 eprintln!("Connection failed");
@@ -39,4 +63,8 @@ pub fn launch<'pktbuf, AsmRef: 'pktbuf, UnixListenerRef: 'pktbuf>(
 {
     let cfg = *config;
     async move { worker(&cfg, &*asm, &*socket).await }
+}
+
+async fn echo(_asm: &Assembly<'_>) -> String {
+    return "hello\n".to_string(); // TODO change the return value of echo
 }
