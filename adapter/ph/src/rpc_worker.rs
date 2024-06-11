9622a6c522b1e1a0f5cb3794c9a20e9c9a8fec38
owenrhysthomas <othomas@appliedinvention.com>
2024-06-11 14:58:16 -0400

    Fixed loop (again) from p-1 to p

--- adapter/ph/src/rpc_worker.rs
+++ adapter/ph/src/rpc_worker.rs
@@ -85,7 +85,7 @@ async fn counters(asm: &Assembly<'_>) -> String {
 
 async fn counters_reset(asm: &Assembly<'_>) -> String {
     for p in 0..2 {
-        asm.counters[p - 1].reset();
+        asm.counters[p].reset();
     }
     return "counters_reset\n".to_string(); // TODO change the return value of counters reset
 }
