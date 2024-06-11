491a682051ce5a56b486589fc22be9fed3c76db1
owenrhysthomas <othomas@appliedinvention.com>
2024-06-11 11:25:00 -0400

    Applied changes: removed config from rpc_workers, removed unnecessary extra JoinSet, removed unused imports

--- adapter/ph/src/rpc_worker.rs
+++ adapter/ph/src/rpc_worker.rs
@@ -1,21 +1,10 @@
 use core::future::Future;
-use std::io::IoSliceMut;
-use tokio::net::UdpSocket;
-use crate::ext::std::vec::VecExt;
-use crate::ext::tokio::net::*;
 use crate::assembly::Assembly;
-use crate::packet::Packet;
 use tokio::net::UnixListener;
-use tokio::net::UnixStream;
 use tokio::io::AsyncWriteExt;
 
-#[derive(Copy, Clone)]
-pub struct Config {
-    pub batch_size: usize
-}
-
 async fn worker(
-    config: &Config, asm: &Assembly<'_>, socket: &UnixListener
+    asm: &Assembly<'_>, socket: &UnixListener
 ) {
 
     loop {
@@ -32,11 +21,10 @@ async fn worker(
 }
 
 pub fn launch<'pktbuf, AsmRef: 'pktbuf, UnixListenerRef: 'pktbuf>(
-    config: &Config, asm: AsmRef, socket: UnixListenerRef)
+    asm: AsmRef, socket: UnixListenerRef)
 -> impl Future<Output = ()> + Send + 'pktbuf
     where AsmRef: std::ops::Deref<Target = Assembly<'pktbuf>> + Send + Sync,
         UnixListenerRef: std::ops::Deref<Target = UnixListener> + Send + Sync
 {
-    let cfg = *config;
-    async move { worker(&cfg, &*asm, &*socket).await }
+    async move { worker(&*asm, &*socket).await }
 }
