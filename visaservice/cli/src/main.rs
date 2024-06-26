7f4560a7fa895f805e09ce7ce1dfa272f5f060a1
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2024-06-25 21:54:45 -0400

    basic test client for visa service

---
+++ visaservice/cli/src/main.rs
@@ -0,0 +1,254 @@
+
+
+
+use clap::{Parser, Subcommand};
+
+
+
+use thrift::protocol::{TBinaryInputProtocol, TBinaryOutputProtocol};
+use thrift::transport::{TFramedReadTransport, TFramedWriteTransport};
+use thrift::transport::{TIoChannel, TTcpChannel};
+use thrift::transport::{ReadHalf, WriteHalf};
+
+
+
+// Tried to use the rust crypt* crates, but could not figure out how to get an HMAC.
+use openssl::sign::Signer;
+use openssl::rsa::Rsa;
+use openssl::pkey::PKey;
+use openssl::hash::MessageDigest;
+
+use std::time::SystemTime;
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub mod vsapi;
+
+use vsapi::{VisaServiceSyncClient, TVisaServiceSyncClient};
+
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Hello {
+        #[arg(short, long)]
+        service: String,
+    },
+    Authenticate {
+        #[arg(short, long)]
+        service: String, // format HOST:PORT
+
+        #[arg(short, long)]
+        claim: Vec<String>,
+
+        #[arg(long)]
+        cert: String, 
+
+        #[arg(long)]
+        key: String, // private key
+    },
+    Deregister {
+        #[arg(short, long)]
+        service: String, // format HOST:PORT
+
+        #[arg(short, long)]
+        apikey: String,
+    },
+}
+
+
+
+
+
+fn main() {
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Hello { service }) => {
+            match hello(&service) {
+                Ok(_) => {
+                    println!("Hello command executed successfully");
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
+        }
+        Some(Commands::Authenticate { service, claim, cert, key }) => {
+            match authenticate(&service, claim, &cert, &key) {
+                Ok(_) => {
+                    println!("Authenticate command executed successfully");
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
+        }
+        Some(Commands::Deregister { service, apikey }) => {
+            match deregister(&service, &apikey) {
+                Ok(_) => {
+                    println!("Deregister command executed successfully");
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
+        }
+        None => {
+            println!("No command provided");
+        }
+    }
+}
+
+// ugh!!
+type VSClientT = VisaServiceSyncClient<
+    TBinaryInputProtocol<
+        TFramedReadTransport<
+            ReadHalf<TTcpChannel>>>,                                    
+    TBinaryOutputProtocol<
+        TFramedWriteTransport<
+            WriteHalf<TTcpChannel>>>>;
+
+
+
+fn newclient(service: &str) -> thrift::Result<VSClientT> {
+    let mut c = TTcpChannel::new();
+    c.open(service)?;
+
+    let (i_chan, o_chan) = c.split()?;
+
+    let i_prot = TBinaryInputProtocol::new(TFramedReadTransport::new(i_chan), true);
+    let o_prot = TBinaryOutputProtocol::new(TFramedWriteTransport::new(o_chan), true);
+
+    Ok(vsapi::VisaServiceSyncClient::new(i_prot, o_prot))
+}
+
+
+
+
+fn hello(service: &str) -> thrift::Result<()> {
+    let mut client = newclient(service)?;
+    match client.hello() {
+        Ok(result) => {
+            println!("HelloResponse:");
+            println!("   session_id: {}", result.session_id.unwrap());
+            println!("   challenge:");
+            if let Some(chal) = result.challenge {
+                println!("      challenge_type: {}", chal.challenge_type.unwrap());
+                if let Some(cdata) = chal.challenge_data {
+                    println!("      challenge_data: {}", hex::encode(cdata));
+                }                                
+            }
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
+    Ok(())
+}
+
+fn authenticate(service: &str, claim: Vec<String>, certFile: &str, privateKeyFile: &str) -> thrift::Result<()> {
+    let mut client = newclient(service)?;
+
+    println!("sending HELLO");
+    let hello_response = client.hello()?;
+    println!("HELLO OK");
+
+
+    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+    let mut attrs = BTreeMap::new();
+    for c in claim {
+        let parts: Vec<&str> = c.splitn(2, '=').collect();
+        if parts.len() == 2 {
+            attrs.insert(parts[0].to_string(), parts[1].to_string());
+        }
+    }    
+
+    let provides = vec![String::from("/zpr/node")];
+
+    // Two IPv6 addresses 
+    let zpraddr =    vec![0xfc, 00, 0x30, 01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+    let tetheraddr = vec![0xfc, 00, 0x30, 02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2];    
+
+    let agent = vsapi::Agent{
+        agent_type: Some(vsapi::AgentType::NODE),
+        attrs: Some(attrs),
+        auth_expires: Some((timestamp + 60*60) as i64),
+        zpr_addr: Some(zpraddr),
+        tether_addr: Some(tetheraddr),
+        ident: Some(String::from("ident-not-generated")), // TODO
+        provides: Some(provides),
+    };
+
+    let mut certfile = File::open(certFile)?;
+    let mut cert_pem_data = String::new();
+    certfile.read_to_string(&mut cert_pem_data)?;
+
+    let mut keyfile = File::open(privateKeyFile)?;
+    let mut key_pem_data = String::new();
+    keyfile.read_to_string(&mut key_pem_data)?;
+
+
+    let hrchal = hello_response.challenge.unwrap();
+    let chal_copy = hrchal.clone(); // we send this one back
+
+    let key = Rsa::private_key_from_pem(key_pem_data.as_bytes()).unwrap();
+    let pkey = PKey::from_rsa(key).unwrap();
+
+    let mut signer = Signer::new(MessageDigest::sha256(), &pkey).unwrap();
+
+    let mut buf = Vec::new();
+    buf.write(&hrchal.challenge_data.unwrap()).unwrap();
+
+    signer.update(&buf).unwrap();
+    signer.update(&timestamp.to_be_bytes()).unwrap();
+    signer.update(&hello_response.session_id.unwrap().to_be_bytes()).unwrap();
+
+    let hmac = signer.sign_to_vec().unwrap();
+    
+    let authreq = vsapi::NodeAuthRequest{
+        session_id: hello_response.session_id,
+        challenge: Some(chal_copy),
+        timestamp: Some(timestamp as i64),
+        node_cert: Some(cert_pem_data.into()),
+        hmac: Some(hmac),
+        node_agent: Some(agent),
+    };
+
+    match client.authenticate(authreq) {
+        Ok(result) => {
+            println!("authenticate sent!");
+            println!("result = {:?}", result);
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
+    
+    Ok(())
+}
+
+
+
+
+fn deregister(service: &str, apikey: &str) -> thrift::Result<()> {
+    let mut client = newclient(service)?;
+    match client.de_register(apikey.into()) {
+        Ok(result) => {
+            println!("de_register sent!");
+            println!("result = {:?}", result);
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
+    Ok(())
+}
