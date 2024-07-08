2a2107789a4b9461796fa6b9d0934a9042c7354e
owenrhysthomas <othomas@appliedinvention.com>
2024-07-08 16:50:06 -0400

    Implemented capture queue, still needs testing

--- adapter/ph/src/queues.rs
+++ adapter/ph/src/queues.rs
@@ -165,7 +165,12 @@ impl<'pktbuf> OutboundSend<'pktbuf> {
     }
 }
 
-// Capture will intercept packets in the PH and dump them into a file for debugging purposes
+#[allow(dead_code)]
+pub enum TryEnqueueError<T> {
+    Full(T),
+}
+
+#[derive(Debug)]
 #[allow(dead_code)]
 pub struct CapPacket<'pktbuf> {
     packet: Packet<'pktbuf>,
@@ -175,11 +180,6 @@ pub struct CapPacket<'pktbuf> {
 #[allow(dead_code)]
 pub struct Capture<'pktbuf> {
     sender: mpsc::Sender<CapPacket<'pktbuf>>,
-}
-
-#[allow(dead_code)]
-pub enum TryEnqueueError<T> {
-    Full(T),
 }
 
 #[allow(dead_code)]
