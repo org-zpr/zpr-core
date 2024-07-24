de5200519364c7310a045de4aab14682e43875a4
owenrhysthomas <othomas@appliedinvention.com>
2024-07-24 10:17:24 -0400

    Added english language name for each direction, not just number

--- tools/link_header_dissector.lua
+++ tools/link_header_dissector.lua
@@ -13,9 +13,20 @@ function zdp_link_p2p_proto.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = zdp_link_p2p_proto.name
 
     local subtree = tree:add(zdp_link_p2p_proto, buffer(), "Direction Data")
-    subtree:add(in_out, buffer(0,1))
+    local direction = buffer(0,1):uint()
+    local direction_name = get_direction_name(direction)
+    subtree:add(in_out, buffer(0,1)):append_text(" (" .. direction_name .. ")")
 
 end
 
 local tcp_port = DissectorTable.get("tcp.port")
 tcp_port:add(59274, zdp_link_p2p_proto)
+
+function get_direction_name(direction)
+    local direction_name = "Unknown"
+
+    if direction == 0 then direction_name = "Inbound"
+    elseif direction == 1 then direction_name = "Outbound" end
+
+    return direction_name
+end
