6fdbf379dacb59cc6821d3ca920062ba81d1f95d
owenrhysthomas <othomas@appliedinvention.com>
2024-07-26 13:28:23 -0400

    Moved type_name_table out of function, fixed mac_addr var name

--- tools/zdp_dissector.lua
+++ tools/zdp_dissector.lua
@@ -26,15 +26,15 @@ function zdp_proto.dissector(buffer, pinfo, tree)
     local subtree = tree:add(zdp_proto, buffer(), "ZDP Header Data")
     subtree:add(zpi_val, buffer(0, 1))
 
-    local type = buffer(2,1):uint()
+    local type = buffer(1,1):uint()
     local type_name = get_type_name(type)
     subtree:add(zdp_type, buffer(1, 1)):append_text(" (" .. type_name .. ")")
 
     subtree:add(excess_len, buffer(2, 1))
     subtree:add(seq_num, buffer(3, 2))
 
-    local type = buffer(1,1):uint()
     local real_len = length - buffer(2,1):uint() 
+    local type = 125
 
     -- Transit Packet
     if type == 0 then
@@ -49,58 +49,18 @@ function zdp_proto.dissector(buffer, pinfo, tree)
         subtree:add(stream_id, buffer(5, 4))
         subtree:add(management_packet, buffer(9, real_len - 21))
         subtree:add(pad, buffer(real_len - 12, 8))
-        subtree:add(mac, buffer(real_len - 4, 4))
+        subtree:add(mac_addr, buffer(real_len - 4, 4))
     -- Other Management Message
     else 
         subtree:add(management_packet, buffer(5, real_len - 17))
         subtree:add(pad, buffer(real_len - 12, 8))
-        subtree:add(mac, buffer(real_len - 4, 4))
+        subtree:add(mac_addr, buffer(real_len - 4, 4))
     end
 
 end
 
 function get_type_name(type)
-    local type_name_table =
-    {
-        [0] = "Transit Packet",
-        [1] = "Unused",
-        [2] = "Destination Unreachable",
-        [3] = "Visa Herald Request",
-        [4] = "Visa Herald Response",
-        [5] = "Visa Update Request",
-        [6] = "Visa Update Response",
-        [7] = "Visa Retract Request",
-        [8] = "Visa Retract Response",
-        [9] = "Visa Deaccept Indication",
-        [10] = "Visa Deaccept ACK",
-        [11] = "Bind Agent Address Request",
-        [12] = "Bind Agent Address Response",
-        [13] = "Unbind Agent Address Request",
-        [14] = "Unbind Agent Address Response",
-        [15] = "Authentication Request",
-        [16] = "Set Path MTU",
-        [17] = "Authentication Response",
-        [127] = "Reserved, Discard",
-        [128] = "ZPR ARP",
-        [129] = "Key Management",
-        [130] = "Discard",
-        [131] = "Echo Request",
-        [132] = "Echo Response",
-        [133] = "Terminate Link or Docking Session Request",
-        [134] = "Terminate Link or Docking Session Response",
-        [135] = "Terminate Link or Docking Session Indication",
-        [136] = "Hello Request",
-        [137] = "Hello Response",
-        [138] = "Configuration Request",
-        [139] = "Configuration Response",
-        [140] = "Register Agent Address Request",
-        [141] = "Not Specified", -- Not specified in RFC
-        [142] = "Register Agent Address Response",
-        [143] = "Unregister Agent Address Request",
-        [144] = "Unregister Agent Address Response",
-        [145] = "Report",
-        [255] = "Reserved, must not be used",
-    }
+    
 
     local type_name = type_name_table[type]
 
@@ -113,6 +73,48 @@ function get_type_name(type)
     return type_name
 
 end 
+
+type_name_table =
+{
+    [0] = "Transit Packet",
+    [1] = "Unused",
+    [2] = "Destination Unreachable",
+    [3] = "Visa Herald Request",
+    [4] = "Visa Herald Response",
+    [5] = "Visa Update Request",
+    [6] = "Visa Update Response",
+    [7] = "Visa Retract Request",
+    [8] = "Visa Retract Response",
+    [9] = "Visa Deaccept Indication",
+    [10] = "Visa Deaccept ACK",
+    [11] = "Bind Agent Address Request",
+    [12] = "Bind Agent Address Response",
+    [13] = "Unbind Agent Address Request",
+    [14] = "Unbind Agent Address Response",
+    [15] = "Authentication Request",
+    [16] = "Set Path MTU",
+    [17] = "Authentication Response",
+    [127] = "Reserved, Discard",
+    [128] = "ZPR ARP",
+    [129] = "Key Management",
+    [130] = "Discard",
+    [131] = "Echo Request",
+    [132] = "Echo Response",
+    [133] = "Terminate Link or Docking Session Request",
+    [134] = "Terminate Link or Docking Session Response",
+    [135] = "Terminate Link or Docking Session Indication",
+    [136] = "Hello Request",
+    [137] = "Hello Response",
+    [138] = "Configuration Request",
+    [139] = "Configuration Response",
+    [140] = "Register Agent Address Request",
+    [141] = "Not Specified", -- Not specified in RFC
+    [142] = "Register Agent Address Response",
+    [143] = "Unregister Agent Address Request",
+    [144] = "Unregister Agent Address Response",
+    [145] = "Report",
+    [255] = "Reserved, must not be used",
+}
 
 local udp_port = DissectorTable.get("udp.port")
 udp_port:add(1201, zdp_proto)
