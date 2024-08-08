12ddf400044305b019358b6b31dc120b8f58610b
Chris Pacejo <cpacejo@appliedinvention.com>
2024-08-07 21:43:03 -0400

    use RCU-ish for flow_control

--- adapter/ph/src/flow_control.rs
+++ adapter/ph/src/flow_control.rs
@@ -1,9 +1,8 @@
+use crate::rcu::RcuBox;
 use cbpf_rs::BpfProgram;
-use std::sync::RwLock;
 
 pub struct FlowControl {
-    // TODO: replace this with RCU
-    inner_control: RwLock<InnerFlow>,
+    inner_control: RcuBox<InnerFlow>,
 }
 
 struct InnerFlow {
@@ -18,23 +17,22 @@ impl FlowControl {
     }
 
     pub fn set_program(&self, program: BpfProgram) {
-        self.inner_control.write().unwrap().flow = Some(program);
+        self.inner_control.write(InnerFlow { flow: Some(program) });
     }
 
     pub fn delete_program(&self) {
-        self.inner_control.write().unwrap().flow = None;
+        self.inner_control.write(InnerFlow { flow: None });
     }
 
     pub fn check_packet(&self, packet: &[u8]) -> u32 {
-        let inner_flow = &self.inner_control.read().unwrap().flow;
-        match inner_flow {
-            Some(program) => program.filter(packet),
-            None => 0,
-        }
+        self.inner_control.inspect(|inner_flow|
+            match &inner_flow.flow {
+                Some(program) => program.filter(packet),
+                None => 0,
+            })
     }
 
     pub fn program_exists(&self) -> bool {
-        let inner_flow = &self.inner_control.read().unwrap().flow;
-        inner_flow.is_some()
+        self.inner_control.inspect(|inner_flow| inner_flow.flow.is_some())
     }
 }
