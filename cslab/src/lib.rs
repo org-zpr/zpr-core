a4b3e9e49a5f1f4bbc9bc05f18ae719232f56dae
owenrhysthomas <othomas@appliedinvention.com>
2024-08-20 12:59:30 -0400

    removed allow unexpected cfgs in favor of lints in Cargo

--- cslab/src/lib.rs
+++ cslab/src/lib.rs
@@ -1,5 +1,4 @@
 //! An RCU-friendly concurrent slab
-#[allow(unexpected_cfgs)]
 #[cfg(loom)]
 use loom::alloc::{self, Layout};
 #[cfg(not(loom))]
@@ -8,7 +7,6 @@ use std::alloc::{self, Layout};
 use std::cell::UnsafeCell;
 //#[cfg(loom)]
 //use loom::cell::UnsafeCell;
-#[allow(unexpected_cfgs)]
 #[cfg(loom)]
 use loom::sync;
 use std::mem::ManuallyDrop;
