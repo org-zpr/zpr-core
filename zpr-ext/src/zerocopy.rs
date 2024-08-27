372173879240c87ca0b1a7169fc4e382388cbcc5
Chris Pacejo <cpacejo@appliedinvention.com>
2024-08-26 20:59:54 -0400

    zpr-ext: eliminate heap allocation for read_from_buf

--- zpr-ext/src/zerocopy.rs
+++ zpr-ext/src/zerocopy.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "bytes")]
 mod zerocopy_bytes {
     use bytes::{Buf, BufMut};
+    use std::mem::MaybeUninit;
     use zerocopy::*;
 
     pub trait AsBytesExt {
@@ -9,6 +10,9 @@ mod zerocopy_bytes {
     }
 
     pub trait FromBytesExt {
+        /// Get a write-only buffer to modify this value.
+        fn as_bytes_mut(&mut self) -> &mut [MaybeUninit<u8>];
+
         /// Like `read_from_prefix()`, but reads from a `Buf`.
         fn read_from_buf(buf: &mut impl Buf) -> Option<Self>
         where
@@ -28,6 +32,18 @@ mod zerocopy_bytes {
     }
 
     impl<T: FromBytes> FromBytesExt for T {
+        fn as_bytes_mut(&mut self) -> &mut [MaybeUninit<u8>] {
+            // SAFETY: we are forming the slice as the correct size;
+            // MaybeUninit<u8> is always safe to cast to, and
+            // T can read anything that's written to it
+            unsafe {
+                std::slice::from_raw_parts_mut(
+                    self as *mut _ as *mut MaybeUninit<u8>,
+                    std::mem::size_of::<T>(),
+                )
+            }
+        }
+
         fn read_from_buf(buf: &mut impl Buf) -> Option<Self>
         where
             Self: Sized,
@@ -35,7 +51,12 @@ mod zerocopy_bytes {
             if buf.remaining() < std::mem::size_of::<Self>() {
                 None
             } else {
-                Self::read_from(buf.copy_to_bytes(std::mem::size_of::<Self>()).as_ref())
+                let mut ret = T::new_zeroed();
+                // SAFETY: we are immediately writing to all bytes
+                buf.copy_to_slice(unsafe {
+                    crate::std::mem::slice_assume_init_mut(ret.as_bytes_mut())
+                });
+                Some(ret)
             }
         }
     }
