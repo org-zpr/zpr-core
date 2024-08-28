b372182b889750b77ca8593dccf9768b446401ea
Chris Pacejo <cpacejo@appliedinvention.com>
2024-08-27 23:05:34 -0400

    more fmt

--- adapter/ph/src/rpc_worker.rs
+++ adapter/ph/src/rpc_worker.rs
@@ -111,11 +111,11 @@ async fn handle_connection(
                 let mut ancillary = SocketAncillary::new(&mut ancillary_buffer);
                 let mut buf = [0; 1]; // Must receive data sent with ancillary data
                 let bufs = &mut [IoSliceMut::new(&mut buf)][..];
-                buf_reader.into_inner().as_ref().recv_vectored_with_ancillary(
-                    bufs,
-                    &mut ancillary,
-                )
-                .await?;
+                buf_reader
+                    .into_inner()
+                    .as_ref()
+                    .recv_vectored_with_ancillary(bufs, &mut ancillary)
+                    .await?;
 
                 // Set capture file using ancillary data
                 buf_writer
