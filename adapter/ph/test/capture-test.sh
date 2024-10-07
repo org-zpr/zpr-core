152c13cfcbe9e5d2712af5b8330e7e78b072281c
Sarah Jaffer <sjaffer@appliedinvention.com>
2024-10-07 15:47:42 -0400

    Fix capture-test

--- adapter/ph/test/capture-test.sh
+++ adapter/ph/test/capture-test.sh
@@ -76,8 +76,7 @@ create_agent_key_and_cert ca node
 #
 sudo -E ip netns exec zpr-node sudo -E -u "$ZPR_USER" "$PH_BIN" \
   --name "zpr-node" --control-path "$NODE_SOCK" \
-  --self-addr 0.0.0.0:12345 --peer-addr1 "$A_SUBSTRATE_ADDR":12345 \
-  --peer-addr2 "$B_SUBSTRATE_ADDR":12345 \
+  --self-addr 0.0.0.0:12345 \
   --ca-file ca.crt --certificate-file node.crt --private-key-file node.key \
   --tun-if tun0 2>&1 |tee node.log &
 CHILDREN=(${CHILDREN[@]} "$!")
@@ -86,7 +85,7 @@ sleep 2
 
 sudo -E ip netns exec zpr-a sudo -E -u "$ZPR_USER" "$PH_BIN" \
   --name "zpr-a" --control-path "$ADAPTER1_SOCK" \
-  --self-addr "$A_SUBSTRATE_ADDR":12345 --peer-addr1 "$NODE_SUBSTRATE_ADDR_A":12345 \
+  --self-addr "$A_SUBSTRATE_ADDR":12345 --node-addr "$NODE_SUBSTRATE_ADDR_A":12345 \
   --ca-file ca.crt --certificate-file adapter1.crt --private-key-file adapter1.key \
   --node-public-key-file node.pubkey \
   --tun-if tun0 2>&1 |tee adapter1.log &
@@ -94,7 +93,7 @@ CHILDREN=(${CHILDREN[@]} "$!")
 
 sudo -E ip netns exec zpr-b sudo -E -u "$ZPR_USER" "$PH_BIN" \
   --name "zpr-b" --control-path "$ADAPTER2_SOCK" \
-  --self-addr "$B_SUBSTRATE_ADDR":12345 --peer-addr1 "$NODE_SUBSTRATE_ADDR_B":12345 \
+  --self-addr "$B_SUBSTRATE_ADDR":12345 --node-addr "$NODE_SUBSTRATE_ADDR_B":12345 \
   --ca-file ca.crt --certificate-file adapter2.crt --private-key-file adapter2.key \
   --node-public-key-file node.pubkey \
   --tun-if tun0 2>&1 |tee adapter2.log &
