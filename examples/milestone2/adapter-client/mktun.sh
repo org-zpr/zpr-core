e75279292370716c85239279d3bf25247723d2df
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2024-12-03 11:50:57 -0500

    script to set up tun interface, updated configs

---
+++ examples/milestone2/adapter-client/mktun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ip tuntap add name tun9 mode tun multi_queue
+ip addr add fd5a:5052:1::3133:7/16 dev tun9
+ip link set tun9 up
---
+++ examples/milestone2/node/mktun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ip tuntap add name tun9 mode tun multi_queue
+ip addr add fd5a:5052:90de::1/32 dev tun9
+ip link set tun9 up
+
