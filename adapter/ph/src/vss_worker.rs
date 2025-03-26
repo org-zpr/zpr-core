39ddfb0b55eea687736471c3e8bb1c22e25df6cc
Sarah Jaffer <sjaffer@appliedinvention.com>
2025-03-26 11:05:34 -0400

    Add some missing error debugging

--- adapter/ph/src/vss_worker.rs
+++ adapter/ph/src/vss_worker.rs
@@ -10,10 +10,16 @@ pub async fn launch(asm: Arc<Assembly>, mut queue: mpsc::Receiver<VSSMsg>) {
     while let Some(msg) = queue.recv().await {
         match msg {
             VSSMsg::PushedVisa(visa) => {
-                let _ = visa_mgmt::parse_visa(&asm, visa);
+                let result = visa_mgmt::parse_visa(&asm, visa);
+                if let Err(e) = result.await {
+                    error!(target: VISA_MGMT, "Error inserting visa: {e}");
+                }
             }
             VSSMsg::PushedRevocation(revocation) => {
-                let _ = visa_mgmt::handle_revocation(&asm, revocation);
+                let result = visa_mgmt::handle_revocation(&asm, revocation);
+                if let Err(e) = result.await {
+                    error!(target: VISA_MGMT, "Error revoking visa: {e}");
+                }
             }
             _ => {
                 error!(target: VISA_MGMT, "received VSS message {msg}; ignoring! (unimplemented)");
