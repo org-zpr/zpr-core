74f709188078bd2971331e97c4945ba2f5c389bf
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2025-07-22 13:07:52 -0400

    namechange

--- libnode/src/claims.rs
+++ libnode/src/claims.rs
@@ -1,3 +1,3 @@
 pub const KATTR_EPID: &str = "zpr.addr";
 pub const KATTR_CN: &str = "device.zpr.adapter.cn";
-pub const KATTR_AAA_NET: &str = "device.zpr.node.aaanet";
+pub const KATTR_AAA_NET: &str = "device.zpr.node.aaa_net";
