4cb80a63987ed8be16e8aaf29a97e4f6448e1e20
Chris Pacejo <cpacejo@appliedinvention.com>
2025-11-19 16:24:28 -0500

    build policy into zpr crate

--- zpr/build.rs
+++ zpr/build.rs
@@ -1,13 +1,19 @@
 fn main() {
     capnpc::CompilerCommand::new()
-        .src_prefix("zpr-vsapi")
-        .file("zpr-vsapi/vs.capnp")
-        .run()
-        .expect("failed to compile zpr-vsapi capnp schema");
-
-    capnpc::CompilerCommand::new()
         .src_prefix("zpr-admin-api")
         .file("zpr-admin-api/cli.capnp")
         .run()
         .expect("failed to compile zpr-admin-api capnp schema");
+
+    capnpc::CompilerCommand::new()
+        .src_prefix("zpr-policy")
+        .file("zpr-policy/policy.capnp")
+        .run()
+        .expect("failed to compile zpr-policy capnp schema");
+
+    capnpc::CompilerCommand::new()
+        .src_prefix("zpr-vsapi")
+        .file("zpr-vsapi/vs.capnp")
+        .run()
+        .expect("failed to compile zpr-vsapi capnp schema");
 }
--- zpr/src/lib.rs
+++ zpr/src/lib.rs
@@ -7,6 +7,11 @@ pub mod admin_api {
     pub use super::cli_capnp as v1;
 }
 
+capnp::generated_code!(pub mod policy_capnp);
+pub mod policy {
+    pub use super::policy_capnp as v1;
+}
+
 capnp::generated_code!(pub mod vs_capnp);
 pub mod vsapi {
     pub use super::vs_capnp as v1;
