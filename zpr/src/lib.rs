e3061447053c6dbea385d668d38f1567820dddd9
Chris Pacejo <cpacejo@appliedinvention.com>
2025-12-08 16:03:25 -0500

    zpr: add feature flags limiting capnp dependencies

--- zpr/src/lib.rs
+++ zpr/src/lib.rs
@@ -4,20 +4,28 @@ pub mod addrs;
 pub mod dn;
 pub mod packet_info;
 pub mod rpc_commands;
+#[cfg(feature = "vsapi")]
 pub mod vsapi_types;
+#[cfg(feature = "vsapi")]
 pub mod vsapi_types_writers;
 
+#[cfg(feature = "admin-api")]
 capnp::generated_code!(pub mod cli_capnp);
+#[cfg(feature = "admin-api")]
 pub mod admin_api {
     pub use super::cli_capnp as v1;
 }
 
+#[cfg(feature = "policy")]
 capnp::generated_code!(pub mod policy_capnp);
+#[cfg(feature = "policy")]
 pub mod policy {
     pub use super::policy_capnp as v1;
 }
 
+#[cfg(feature = "vsapi")]
 capnp::generated_code!(pub mod vs_capnp);
+#[cfg(feature = "vsapi")]
 pub mod vsapi {
     pub use super::vs_capnp as v1;
 }
